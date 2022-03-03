f7f38f2465aac0f2085fc62cc1e2eec291764a7d
galangdavianpradana <galangdavianp>
2022-03-03 14:18:13 +0700

    🍊

--- server.js
+++ server.js
@@ -2,36 +2,50 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-
     const url = req.url
 
-    const input = (skipp, res) => {
-        fs.readFile(skipp, (err, datanya) => {
-            err? res.write('Error : File Not Found') : res.write(datanya);
+    const renderHTML = (path, res) => {
+        console.log(path)
+        res.setHeader('Content-Type', 'text/html')
+        fs.readFile(path, (err, datanya) => {
+            
+            if(err){
+                res.writeHead(404)
+                res.write('Error, Page Not Found')
+            }else{
+                console.log(datanya)
+                res.write(datanya.toString())
+            }
             res.end()
         })
     } 
-
+    console.log(url.toLowerCase())
     switch (url.toLowerCase()) {
-        case '/index' :
-            input('./index.html', res)
-            break;
-        case '/about' :
-            input('./about.html', res)
-            break;
-        case '/contact' :
-            input('./form.html', res)
+        case '/':
+            renderHTML('index.html', res)
             break;
         case '/experience' :
-            input('./service.html', res)
+            renderHTML('./experience.html', res)
             break;
+        case '/about' :
+            renderHTML('./about.html', res)
+            break;
+        case '/contact' :
+            renderHTML('./form.html', res)
+            break;
+        case '/experience' :
+            renderHTML('./service.html', res)
+            break;
+        case '/assets/style.css':
+            res.setHeader('Content-Type', 'text/css')
+            const fileContents = fs.readFileSync('Assets/style.css');
+            console.log(fileContents)
+            res.end(fileContents);
+            break
         default:
-            input('./contact.html', res)
     }
-
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
+    res.end()
+    
 
 }).listen(3000, () => {
     console.log('Server Listen = 3000')
